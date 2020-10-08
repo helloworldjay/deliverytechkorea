7fed09cf3fc2478faae14c6d6bb274738c601cd0
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-10-08 13:04:29 +0900

    edit profile

--- yogiyo/accounts/urls.py
+++ yogiyo/accounts/urls.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path('login/', views.login, name="login"), # /accounts/login/ => settings.LOGIN_URL
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
+    path('edit/',views.profile_edit, name= 'profile_edit'),
 ]
--- yogiyo/accounts/views.py
+++ yogiyo/accounts/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, logout_then_login
-from .forms import SignupForm
+from .forms import SignupForm, ProfileForm
 from django.contrib import messages
+
 
 # Create your views here.
 
@@ -25,4 +27,19 @@ def signup(request):
         form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
+    })
+
+# 항상 로그인 상황이어야되므로 장식자를 붙여준다, import 필요
+@login_required
+def profile_edit(request):
+    if request.method=='POST':
+        form = ProfileForm(request.POST, request.FILES,instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "프로필을 수정/저장했습니다.")
+            return redirect("profile_edit")
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, "accounts/profile_edit_form.html", {
+        "form" : form,
     })
