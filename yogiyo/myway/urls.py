73a317b841c6bd3a82b0b6e085794934494f7253
Jay Seungjin Baek <sj.seungjin.baek@gmail.com>
2020-10-17 15:03:03 +0900

    remake web page

--- yogiyo/myway/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'myway'
-
-urlpatterns = [
-    path('post/new/', views.post_new, name='post_new'),
-    path('post/<int:pk>/',views.post_detail, name = 'post_detail'),
-]
